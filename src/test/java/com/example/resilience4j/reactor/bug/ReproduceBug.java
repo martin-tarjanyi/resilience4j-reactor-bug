d4a39711f71405583a67bdae05501efbe5433134
Martin Tarjanyi <tmartin193@gmail.com>
2019-05-19 17:24:53 +0200

    Initial commit

---
+++ src/test/java/com/example/resilience4j/reactor/bug/ReproduceBug.java
@@ -0,0 +1,36 @@
+package com.example.resilience4j.reactor.bug;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.reactor.circuitbreaker.operator.CircuitBreakerOperator;
+import org.junit.Test;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+import reactor.util.context.Context;
+
+public class ReproduceBug
+{
+    @Test
+    public void reactorContextShouldBeAvailable() // this test passes
+    {
+        Mono<String> stringMono = Mono.just("myData")
+                                      .doOnEach(signal -> System.out.println(signal.getContext().<String>get("key")))
+                                      .subscriberContext(Context.of("key", "value"));
+
+        StepVerifier.create(stringMono)
+                    .expectNext("myData")
+                    .verifyComplete();
+    }
+
+    @Test
+    public void reactorContextShouldBeAvailableWithResilience4jCircuitBreaker() // this test fails
+    {
+        Mono<String> stringMono = Mono.just("myData")
+                                      .doOnEach(signal -> System.out.println(signal.getContext().<String>get("key")))
+                                      .compose(CircuitBreakerOperator.of(CircuitBreaker.ofDefaults("myRequest")))
+                                      .subscriberContext(Context.of("key", "value"));
+
+        StepVerifier.create(stringMono)
+                    .expectNext("myData")
+                    .verifyComplete();
+    }
+}
